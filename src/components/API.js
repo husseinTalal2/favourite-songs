05ec7e5638908652f26178cad3e6402913d03e8c
evan hameed <evanhameed9966@gmail.com>
2020-07-11 02:15:34 +0300

    Adding a comment

--- src/components/API.js
+++ src/components/API.js
@@ -3,6 +3,9 @@ const API = {
   fetch: fetchSearch,
 };
 
+// Doing the fetching this way since the search endpoint 
+// is the only endpoint we are going to be working on in this project
+
 function fetchSearch(query) {
   let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDCqqJyOBIzbH3xwrgclI1DXRSqqNOuRUw&type=video&q=${query}`;
 
