cb269934fad33629b835eccb4db1f5cb6b004234
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-07-11 21:53:48 +0300

    adding sign up functionality

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {
     MDBContainer,
@@ -10,12 +10,27 @@ import {
     MDBInput,
 } from "mdbreact";
 import image from "../images/31501.jpg";
+import {actions} from "../AccountActions";
+import { Link } from 'react-router-dom';
+
 function SignUp() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const handleEmail = (e)=> {
+        setEmail(e.target.value)
+    }
+    const handlePassword = (e)=> {
+        setPassword(e.target.value)
+    }
+    const handleSignUp = (e)=> {
+        e.preventDefault();
+        actions.signUp(email, password);
+    }
     return (
         <MDBContainer className="my-5">
         <MDBRow className="justify-content-center ">
             <MDBCol md="6">
-                <img className="img-fluid pr-5" src={image} />
+                <img className="img-fluid pr-5" src={image} alt="cover" />
             </MDBCol>
             <MDBCol md="5">
                 <MDBCard>
@@ -33,6 +48,7 @@ function SignUp() {
                                 validate
                                 error="wrong"
                                 success="right"
+                                onChange={handleEmail}
                             />
                             <MDBInput
                                 className="black-text "
@@ -42,16 +58,19 @@ function SignUp() {
                                 group
                                 type="password"
                                 validate
+                                onChange={handlePassword}
                             />
 
                             <div className="text-center py-1 ">
+                                <Link to="/">
                                 <MDBBtn
                                     className="aqua-gradient"
                                     type="submit"
+                                    onClick={handleSignUp}
                                 >
                                     Sign Up
                                 </MDBBtn>
-                               
+                                </Link>
                             </div>
                         </form>
                     </MDBCardBody>
